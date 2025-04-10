2e86b7c5f8ebb727da14f9e2cc33196b45ac780f
pupubear007 <111906408+pupubear007@users.noreply.github.com>
2025-04-10 03:25:24 -0500

    Add files via upload

---
+++ my_test/md2folder_folder2md.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+chmod +x folder_md_converter.py
+./folder_md_converter.py to-folder -i project_structure.md -o new_project/
+./folder_md_converter.py to-md my_project -o project_structure_new.md
